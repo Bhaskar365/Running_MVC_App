221f75614729b04cccf87e99d08a9e2977236c69
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-13 22:27:03 +0530

    home index page

--- RunnningWebApp_MVC/obj/Debug/net6.0/RunnningWebApp_MVC.AssemblyInfo.cs
+++ RunnningWebApp_MVC/obj/Debug/net6.0/RunnningWebApp_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunnningWebApp_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f511357e61a2618489a0e70d839d3b1e1092d01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a9c9eb9fd1fd33717a058e9d26d4e40cfd7f5da")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunnningWebApp_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunnningWebApp_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
