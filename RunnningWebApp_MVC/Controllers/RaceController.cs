2a9c9eb9fd1fd33717a058e9d26d4e40cfd7f5da
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-13 22:15:39 +0530

    controllers added

---
+++ RunnningWebApp_MVC/Controllers/RaceController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RunnningWebApp_MVC.Controllers
+{
+    public class RaceController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
