f9726f594dc4622d7b27b4dfe780f744bb737333
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-14 21:51:20 +0530

    repositories added

--- RunnningWebApp_MVC/Program.cs
+++ RunnningWebApp_MVC/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using RunnningWebApp_MVC.Data;
+using RunnningWebApp_MVC.Interfaces;
+using RunnningWebApp_MVC.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
 });
+
+builder.Services.AddScoped<IClubRepository, ClubRepository>();
+builder.Services.AddScoped<IRaceRepository, RaceRepository>();  
 
 var app = builder.Build();
 
--- RunnningWebApp_MVC/obj/Debug/net6.0/RunnningWebApp_MVC.AssemblyInfo.cs
+++ RunnningWebApp_MVC/obj/Debug/net6.0/RunnningWebApp_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunnningWebApp_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+360d13af83d4b6a85d7d4ec97608e40a5505ae61")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunnningWebApp_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunnningWebApp_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
