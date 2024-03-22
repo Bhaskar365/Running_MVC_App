0655636a46b91ef4eacebf2ad38aad0458884ca9
Bhaskar365 <bhaskarjyotidas365@gmail.com>
2024-03-22 17:47:33 +0530

    delete code added to club controller and delete view in club added

--- RunnningWebApp_MVC/obj/Debug/net6.0/RunnningWebApp_MVC.AssemblyInfo.cs
+++ RunnningWebApp_MVC/obj/Debug/net6.0/RunnningWebApp_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunnningWebApp_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+815cce62c67ca2e1b98d2f1352b3602133d65f06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96791bdca57c5ad7a450cbe683274140b088bdc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunnningWebApp_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunnningWebApp_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
